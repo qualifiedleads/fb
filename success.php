a08c743961b41d9a68ab33bf803d34d90be6da37
Thomas Henley <thomas@hendore.co.uk>
2013-07-26 02:28:17 +0100

    Fixed success page

--- success.php
+++ success.php
@@ -1,12 +1,71 @@
-<h2>Thank you</h2>
-<p>This page should contain the google tags, and is only sent to the user when contact form was filled in properly
-Navigating to sitename.com/mail.php or not filling in all form fields will redirect to index and give the user a
-message about invalid input</p>
+<!DOCTYPE html>
+<html xmlns:fb="http://ogp.me/ns/fb#" class="no-js" itemscope="" itemtype="http://schema.org/Product" dir="rtl" lang="he"> 
+  <head>
+    <title>Thank You</title>
 
-<!-- Google Tags (Only included on success) -->
-<?php if(isset($contactMailSent) && $contactMailSent === true): ?>
-    <noscript>
-        <iframe src="//www.googletagmanager.com/ns.html?id=GTM-23J6" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    </noscript>
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-23J6');</script>
-<?php endif; ?>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
+
+    <?php if(isset($contactMailSent) && $contactMailSent === true): ?>
+        <!-- Fb conversion tracking  -->
+        <script type="text/javascript">
+        var fb_param = {};
+        fb_param.pixel_id = '6008816727785';
+        fb_param.value = '0.00';
+        fb_param.currency = 'USD';
+        (function(){
+          var fpw = document.createElement('script');
+          fpw.async = true;
+          fpw.src = '//connect.facebook.net/en_US/fp.js';
+          var ref = document.getElementsByTagName('script')[0];
+          ref.parentNode.insertBefore(fpw, ref);
+        })();
+        </script>
+        <!-- end of Fb conversion tracking  -->
+    <?php endif; ?>
+
+    <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/offsite_event.php?id=6008816727785&amp;value=0&amp;currency=USD" /></noscript>
+    <link rel="stylesheet" href="stylesheets/base.css" media="Screen" type="text/css" />
+    <link rel="stylesheet" href="stylesheets/mobile.css" media="handheld, only screen and (max-width: 480px), only screen and (max-device-width: 480px)" type="text/css" />
+
+    <!--[if IEMobile]>
+    <link rel="stylesheet" href="mobile.css" media="screen" type="text/css"  />
+    <![endif]-->
+
+    <!-- These are Open Graph tags.  They add meta data to your  -->
+    <!-- site that facebook uses when your content is shared     -->
+    <!-- over facebook.  You should fill these tags in with      -->
+    <!-- your data.  To learn more about Open Graph, visit       -->
+    <!-- 'https://developers.facebook.com/docs/opengraph/'       -->
+    <meta property="og:title" content="Concept ac LP" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://apps.facebook.com/conceptac_lp/" />
+    <meta property="og:image" content="https://limitless-fortress-6366.herokuapp.com/logo.png" />
+    <meta property="og:site_name" content="Concept ac LP" />
+    <meta property="og:description" content="My first app" />
+    <meta property="fb:app_id" content="621963677837780" />
+   </head>
+    <body>
+    
+        <!-- Google Tag Manager -->
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-23J6"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-23J6');</script>
+        <!-- End Google Tag Manager -->
+
+        <h1> קריירה בעיצוב מתחילה בקונספט</h1>
+
+        <h2>אנו מודים לך על פנייתך, ברגעים אלו נשלח לתיבת האימייל שלך קישור שעלייך ללחוץ עליו על מנת לאמת נתונים</h2>
+        <h3>נשמח לעמוד לרשותך בכל עת, צוות קונספט המכללה לעיצוב</h3>
+
+        <footer id="footer">
+            <p><strong>קונספט המכללה לעיצוב</strong> רח’ שפע טל 10 <strong>(200 מטר מעזריאלי)</strong> חניה חינם ללא הגבלה  &nbsp <br>
+            <span><strong><a href="tel:03-771-3026" onClick="_gaq.push(['_trackPageview', '/vpv/mehinahtml/footer/ClickToCall']);" >טלפון:03-761-3027</a></strong></span>
+        </footer>
+        
+    </body>
+</html>
