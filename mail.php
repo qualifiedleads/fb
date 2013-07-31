a46e2b466e7a071eb9232a8616612c658489e50a
Jen Brannstrom <jen.brannstrom@morequalifiedleads.co.uk>
2013-07-31 22:20:44 +0300

    removed closing PHP tag

--- mail.php
+++ mail.php
@@ -12,16 +12,15 @@
     } else {
         // Got here? Then user submitted data for each required field
         // Compose the email to send
-        $subject = 'Facebook lead';
+        $subject = 'Fb Mehina lead';
         $body    = sprintf('שם: %s נייד: %s דוא"ל:  %s פרטים: %s', $name, $phone, $email, $uni);
         $from    = 'From:' . $email;
 
         // Send the email
 //        mail('info@concept-academy.co.il', $subject, $body, $from);
 //        mail('leadsbackup@conce.pt',       $subject, $body, $from);
-          mail('web@concept-academy.co.il',          $subject, $body, $from);
+          mail('web@concept-academy.co.il',  $subject, $body, $from);
 
         $contactMailSent = true;
         include 'success.php';
     }
-?>
